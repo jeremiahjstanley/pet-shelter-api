9c67ede2395d1058c97d0e0cec175db7f81a78ad
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2020-03-09 11:14:16 -0600

    Add cors for client

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors());
 
 app.locals.title = "Pet Shelter API";
 
