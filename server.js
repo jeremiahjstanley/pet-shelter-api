e97ac18d1043414338b51f7bb7cb6dd9b6511ca2
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2020-03-09 08:52:28 -0600

    Pivot from mongodb to postgres

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const schema = require('./schema/schema');
+const schema = require("./schema/schema");
 
 const app = express();
-
-app.use(cors());
 
 app.locals.title = "Pet Shelter API";
 
 app.set("port", process.env.PORT || 4000);
-
-mongoose.connect('', {useNewUrlParser: true})
-
-mongoose.connection.once('open', () => {
-    console.log(`${app.locals.title} is connected to the database`);
-});
 
 app.use(
   "/graphql",
@@ -29,4 +19,3 @@ app.use(
 app.listen(app.get("port"), () => {
   console.log(`${app.locals.title} is running on ${app.get("port")}.`);
 });
- 
