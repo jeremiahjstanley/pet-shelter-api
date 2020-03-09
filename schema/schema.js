f08a0b02d2610eeb87c7615cfd99b5e536a0f903
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2020-03-09 08:56:18 -0600

    Separate types and queries from schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,47 +1,10 @@
 const graphql = require("graphql");
-const Pet = require("../models/pet.js");
+const RootQuery = require("./queries/rootquery");
+const RootMutation = require("./mutations/rootmutation");
 
-const {
-  GraphQLID,
-  GraphQLInt,
-  GraphQLList,
-  GraphQLObjectType,
-  GraphQLSchema,
-  GraphQLString
-} = graphql;
-
-const PetType = new GraphQLObjectType({
-  name: "Pet",
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    type: { type: GraphQLString },
-    breed: { type: GraphQLString },
-    location: { type: GraphQLString },
-    latitude: { type: GraphQLInt },
-    longitude: { type: GraphQLInt }
-  })
-});
-
-const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
-  fields: {
-    pet: {
-      type: PetType,
-      args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        return Pet.findById(args.id); 
-      }
-    },
-    pets: {
-      type: new GraphQLList(PetType),
-      resolve(parent, args) {
-        return Pet.find({})
-      }
-    }
-  }
-});
+const { GraphQLSchema } = graphql;
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: RootMutation
 });
